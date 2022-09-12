1db2465065e4817e9971267e23b39ed294ef6c36
Samantha Guerra <88997930+Sam-Antics@users.noreply.github.com>
2022-09-11 23:39:12 -0500

    added getAll() functionality

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,22 +13,40 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
+// ✅Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
   console.log('PUT to samantext db');
-
+  
+  // connect to database
   const samantextDb = await openDB('samantext_db', 1);
 
+  // create transaction
   const tx = samantextDb.transaction('samantext', 'readwrite');
 
+  // open the desired object store
   const store = tx.objectStore('samantext');
 
+  // use the .put() method to update an object
   const request = store.put({ id: 1, value: content });
   const result = await request;
   console.log('data saved to samantext', result);
 };
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+// ✅Add logic for a method that gets all the content from the database
+export const getDb = async () => {
+  console.log('GET from samantext');
+
+  const samantextDb = await openDB('samantext_db', 1);
+
+  const tx = samantextDb.transaction('samantext', 'readonly');
+
+  const store = tx.objectStore('samantext');
+
+  // use the .getAll() method to get all data in the database
+  const request = store.getAll();
+  const result = await request;
+  console.log('result.value', result);
+  return result;
+}
 
 initdb();
